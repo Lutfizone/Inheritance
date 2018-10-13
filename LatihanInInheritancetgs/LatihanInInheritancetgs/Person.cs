09303af305626b7a0ded8636b990b569e3c907bd
Lutfi Dian <lutfidiann@gmail.com>
2018-10-13 20:02:43 +0700

    Add files via upload

---
+++ LatihanInInheritancetgs/LatihanInInheritancetgs/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInInheritancetgs
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Addres { get; set; }
+        public string Identitas { get; set; }
+
+        public void Ngomong()
+        {
+            Console.WriteLine("Nama saya adalah : {0}", Name);
+        }
+
+        public void TempatId()
+        {
+            Console.WriteLine("Saya adalah {0}, saya seorang {1}", Name,Identitas);
+           
+        }
+
+        public void StatusId()
+        {
+            Console.WriteLine("Saya seorang {0} , dan nama saya adalah {1}", Identitas, Name);
+        }
+
+
+    }
+}
---
+++ LatihanInInheritancetgs/LatihanInInheritancetgs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInInheritancetgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Beckham";
+            person.Identitas = "Pemain Sepak Bola";
+            person.Ngomong();
+            person.TempatId();
+
+            var student = new Student();
+            student.Name = "Donald Trump";
+            student.Identitas = "President";
+            student.StatusId();
+        }
+    }
+}
